73db6dce1ee5d82f91e58bac89c4574e3a111738
Shreyan Das <hrijudas@hotmail.com>
2022-07-06 18:55:14 +0530

    Contact US Done

--- src/contactPageLoad.js
+++ src/contactPageLoad.js
@@ -3,12 +3,19 @@ const contactPageLoad = (container) => {
     const layout = document.createElement("div")
   
     const heading = document.createElement("h1")
+    const p = document.createElement("p")
+    p.textContent = "Contact us on abc@xyz.com or call us on 1234567890"
+    const address = document.createElement("p")
+    address.textContent = "Find Us at Point Nemo, Pacific Ocean"
     heading.classList.add("heading")
     heading.textContent = "Contact Us"
+    
     layout.appendChild(heading)
-  
+    layout.appendChild(p)
+    layout.appendChild(address)
   
     container.appendChild(layout)
 } 
+
 
 export default contactPageLoad
